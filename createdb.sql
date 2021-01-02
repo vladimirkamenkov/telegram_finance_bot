179a9a295b12c0db38ce7fd093b43a77bb8a6a42
vladimir-kamenkov <vladimir.kamenkov@moneyman.ru>
2021-01-02 21:07:47 +0300

    done

---
+++ createdb.sql
@@ -0,0 +1,36 @@
+create table budget(
+    codename varchar(255) primary key,
+    daily_limit integer
+);
+
+create table category(
+    codename varchar(255) primary key,
+    name varchar(255),
+    is_base_expense boolean,
+    aliases text
+);
+
+create table expense(
+    id integer primary key,
+    amount integer,
+    created datetime,
+    category_codename integer,
+    raw_text text,
+    FOREIGN KEY(category_codename) REFERENCES category(codename)
+);
+
+insert into category (codename, name, is_base_expense, aliases)
+values
+    ("products", "продукты", true, "еда"),
+    ("coffee", "кофе", true, ""),
+    ("dinner", "обед", true, "столовая, ланч, бизнес-ланч, бизнес ланч"),
+    ("entertainment", "развлечения", true, "развлечения, ресторан, рест, кафе, кино"),
+    ("transport", "транспорт", false, "транспорт, метро, автобус, metro, такси, taxi"),
+    ("phone", "телефон", false, "теле2, связь, телефон"),
+    ("books", "книги", false, "литература, литра, лит-ра"),
+    ("internet", "интернет", false, "инет, inet"),
+    ("subscriptions", "подписки", false, "подписка"),
+    ("other", "прочее", true, "");
+
+
+insert into budget(codename, daily_limit) values ('base', 50);
--- server.py
+++ server.py
@@ -1,8 +1,110 @@
 import logging
 import os
 
+import exceptions
+import expenses
+from categories import Categories
+
 from aiogram import Bot, Dispatcher, executor, types
+
+
+logging.basicConfig(level=logging.INFO)
 
 API_TOKEN = os.getenv("FINANCE_BOT_API_TOKEN")
 
-print(API_TOKEN)
+# Initialize bot and dispatcher
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
+
+def auth(func):
+    async def wrapper(message):
+        if message['from']['id'] != 306229415 and message['from']['id'] != 639704213:
+            return await message.reply("Access denied", reply=False)
+        return await func(message)
+
+    return wrapper
+
+
+@dp.message_handler(commands=['start', 'help'])
+@auth
+async def send_welcome(message: types.Message):
+    """Отправляет приветственное сообщение и помощь по боту"""
+    await message.answer(
+        "Бот для учёта финансов\n\n"
+        "Добавить расход: 12 такси\n"
+        "Сегодняшняя статистика: /today\n"
+        "За текущий месяц: /month\n"
+        "Последние внесённые расходы: /expenses\n"
+        "Категории трат: /categories")
+
+
+@dp.message_handler(lambda message: message.text.startswith('/del'))
+async def del_expense(message: types.Message):
+    """Удаляет одну запись о расходе по её идентификатору"""
+    row_id = int(message.text[4:])
+    expenses.delete_expense(row_id)
+    answer_message = "Удалил"
+    await message.answer(answer_message)
+
+
+@dp.message_handler(commands=['categories'])
+@auth
+async def categories_list(message: types.Message):
+    """Отправляет список категорий расходов"""
+    categories = Categories().get_all_categories()
+    answer_message = "Категории трат:\n\n* " +\
+            ("\n* ".join([c.name+' ('+", ".join(c.aliases)+')' for c in categories]))
+    await message.answer(answer_message)
+
+
+@dp.message_handler(commands=['today'])
+@auth
+async def today_statistics(message: types.Message):
+    """Отправляет сегодняшнюю статистику трат"""
+    answer_message = expenses.get_today_statistics()
+    await message.answer(answer_message)
+
+
+@dp.message_handler(commands=['month'])
+@auth
+async def month_statistics(message: types.Message):
+    """Отправляет статистику трат текущего месяца"""
+    answer_message = expenses.get_month_statistics()
+    await message.answer(answer_message)
+
+
+@dp.message_handler(commands=['expenses'])
+@auth
+async def list_expenses(message: types.Message):
+    """Отправляет последние несколько записей о расходах"""
+    last_expenses = expenses.last()
+    if not last_expenses:
+        await message.answer("Расходы ещё не заведены")
+        return
+
+    last_expenses_rows = [
+        f"{expense.amount} руб. на {expense.category_name} — нажми "
+        f"/del{expense.id} для удаления"
+        for expense in last_expenses]
+    answer_message = "Последние сохранённые траты:\n\n* " + "\n\n* "\
+            .join(last_expenses_rows)
+    await message.answer(answer_message)
+
+
+@dp.message_handler()
+@auth
+async def add_expense(message: types.Message):
+    """Добавляет новый расход"""
+    try:
+        expense = expenses.add_expense(message.text)
+    except exceptions.NotCorrectMessage as e:
+        await message.answer(str(e))
+        return
+    answer_message = (
+        f"Добавлены траты {expense.amount} руб на {expense.category_name}.\n\n"
+        f"{expenses.get_today_statistics()}")
+    await message.answer(answer_message)
+
+
+if __name__ == '__main__':
+    executor.start_polling(dp, skip_updates=True)
