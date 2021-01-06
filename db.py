67f235644931f6bc96de46c0da34cb18512bfb5e
vladimir-kamenkov <vladimir.kamenkov@moneyman.ru>
2021-01-06 23:40:23 +0300

    change path to db

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from typing import Dict, List, Tuple
 
 import sqlite3
 
-conn = sqlite3.connect(os.path.join("finance.db"))
+conn = sqlite3.connect("/opt/apps/finance-bot/finance.db")
 cursor = conn.cursor()
 
 
